9ad978dbe360deff3df985a13bdd10b949b1699e
giane pena <151689433+GianePena@users.noreply.github.com>
2025-08-25 12:23:43 -0300

    ejercitacion estructuras condicionantes

--- trabajo_en_clase/ejercicios_condicionantes.py
+++ trabajo_en_clase/ejercicios_condicionantes.py
@@ -11,7 +11,7 @@ mes=int(dia_mes[1])
 
 # Dia de semana: 
 dia_format=dia.lower()
-if dia_format=="lunes" or dia_format=="martes" or dia_format=="miercoles" or dia_format=="jueves" or dia_format=="viernes" or dia_format=="sabado" or dia_format=="domingo":
+if dia_format!="lunes" or dia_format!="martes" or dia_format!="miercoles" or dia_format!="jueves" or dia_format!="viernes" or dia_format!="sabado" or dia_format!="domingo":
     print("Día inválido:", dia)
 elif dia_format=="sabado" or dia_format=="domingo":
     print("Día incorrecto: corresponde a fin de semana")
@@ -19,13 +19,13 @@ else:
     print("DIA DE SEMANA: ", dia)
 
 #Dia de mes: menor a 30 y mayor a 0
-if dia_del_mes<0 or dia_del_mes>30:
+if dia_del_mes<=0 or dia_del_mes>30:
     print("Dia del mes ingresado icorrecto")
 else:
     print("DIA DE MES: ", dia_del_mes)
 
 #Dia mes: menor a 12 y mayor a 0
-if mes<0 or mes>12:
+if mes<=0 or mes>12:
     print("Mes ingresado incorrcto")
 else: 
     print("MES: ", mes)
@@ -40,14 +40,14 @@ niveles={
 }
 
 if dia_format =="lunes" or dia_format=="martes" or dia_format=="miercoles":
-    print("Se romara examen")
+    print(f"El dia {dia_format} se tomo examen")
     numero_de_aprobados=int(input("Ingrese el numero de alumno aprobados: "))
-    numero_de_desaporbados=int(input("Ingrese el numero de desaprobados: "))
-    total_de_alumnos= numero_de_aprobados+numero_de_desaporbados
-    procentajes_de_desaprobados=(numero_de_desaporbados*100)/total_de_alumnos
-    print(f"El porcentade de alumnos desaprobado el dia lunes es de %{procentajes_de_desaprobados} ")
-    procentajes_de_aprobados=(numero_de_aprobados)*100/total_de_alumnos
-    print(f"El porcentade de alumnos aprobado el dia lunes es de %{procentajes_de_aprobados} ")
+    numero_de_desaprobados=int(input("Ingrese el numero de desaprobados: "))
+    total_de_alumnos= numero_de_aprobados+numero_de_desaprobados
+    porcentajes_de_desaprobados=(numero_de_desaprobados*100)/total_de_alumnos
+    print(f"El porcentade de alumnos desaprobado el dia  es de %{porcentajes_de_desaprobados} ")
+    porcentajes_de_aprobados=(numero_de_aprobados)*100/total_de_alumnos
+    print(f"El porcentade de alumnos aprobado el dia es de %{porcentajes_de_aprobados} ")
 else:
     if dia_format == "jueves":
         print("Ese día no se tomarán exámenes")
